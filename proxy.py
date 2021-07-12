a9dd19400527b6bfcf2b6ac7d70a17154aa6a2bc
Matheus <mlopes.matheus@gmail.com>
2021-07-11 23:52:22 -0400

    moving file

--- proxy.py
+++ proxy.py
@@ -1,14 +1,24 @@
 import os.path
+import shutil
 import time
 
-FILE_PATH = '/home/matheus.lopes/media_files/file_2.img'
+HOME_PATH = '/home/matheus/'
+
+FILE_ORIGIN = HOME_PATH + 'proxy_files/origin/'
+FILE_DEST = HOME_PATH + 'proxy_files/dest/'
+
+FILE_NAME = 'file_4'
 
 print('======= bom dia =======')
 
-while not os.path.exists(FILE_PATH):
-    print('Ainda não existe')
-    time.sleep(0.5)
-if os.path.isfile(FILE_PATH):
-    print('existeee ****')
+while not os.path.exists(FILE_ORIGIN + FILE_NAME):
+    print('Ainda não existe,')
+    time.sleep(1)
+if os.path.isfile(FILE_ORIGIN + FILE_NAME):
+    print('Arquivo encontrado.')
+    print('movendo para: ' + FILE_DEST)
+    shutil.move(FILE_ORIGIN + FILE_NAME, FILE_DEST)
+
 else:
-    raise ValueError(("%s isn't a file!" % FILE_PATH))
+    raise ValueError(("%s isn't a file!" % FILE_ORIGIN))
+
